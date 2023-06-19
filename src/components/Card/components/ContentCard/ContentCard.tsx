3ad8fc24e99ad1f8658a2767aee42eb5058f7235
Josiel Souza <souzajosiel611@gmail.com>
2023-06-19 02:57:39 -0300

    fix: urls git

--- src/components/Card/components/ContentCard/ContentCard.tsx
+++ src/components/Card/components/ContentCard/ContentCard.tsx
@@ -34,11 +34,11 @@ export const ContentCard: Array<Icard> = [
     img4: `${path}/imgApiLojaDeAnuncios/vscode.png`,
     description: `Essa API foi feita para atender o front end Anúncios de Carros, utilizei o node, javascript, typescript, jest para os testes, postman api  para documentação da API, podendo acessá-la por aqui https://documenter.getpostman.com/view/16102831/2s93Xzyi9E,  axios para requisições https, planejo evoluir esse projeto adicionando uma ORM, DOCKER e mais funcionalidades. Foi feito deploy na render.com. Para visualizar melhor os objetos da API no navegar é preciso ter instalado no navegador a extensão JSON Viewer.`,
     site: "https://api-loja-carro.onrender.com/",
-    codigo: "https://github.com/josielsouza/api-loja-carro",
+    codigo: "https://github.com/jsz611/car-shop-api",
   },
   {
     id: "3",
-    name: "Anúncios de Carros",
+    name: "Site de Anúncios de Carros",
     img1: `${path}/imgLoja/home.png`,
     img2: `${path}/imgLoja/cadastro.png`,
     img3: `${path}/imgLoja/card.png`,
@@ -46,7 +46,7 @@ export const ContentCard: Array<Icard> = [
     description:
       "Esse projeto foi feito com react vite com typescript, styled-component, react-router-dom, lib Ether para manipulação de dados na blockchain na rede da BINANCE, usando a metamask como conexão. Tendo como objetivo um crud e transferência de token para outra carteira. E foi feito deploy na netlify. Também fiz uma API para este projeto, para fins de teste você pode abrir o proxímo link ver site do API Anúncio de Carro, para cada alteração é preciso atualizar a página da api, assim você pode criar outros anúncios, editar ou deletar. Para usar a carteira metamask é preciso tê-la instalada no seu navegador.",
     site: "https://josiel-auto-car.netlify.app/",
-    codigo: "https://github.com/josielsouza/Anuncios-de-Carros",
+    codigo: "https://github.com/jsz611/car-ad-website",
   },
   {
     id: "4",
@@ -79,6 +79,6 @@ export const ContentCard: Array<Icard> = [
     img4: `${path}/imgApiProduto/Unico-produto.png`,
     description: `Uma API simples, apenas para consumo de produto, aceita requisição http GET. Construída apenas com node, nodemon para  monitorar arquivos do projeto em busca de mudanças e reiniciar automaticamente o servidor toda vez que algum arquivo é modificado, e foi deployada na vercel.`,
     site: "https://api-estoque-produto.vercel.app/",
-    codigo: "https://github.com/josielsouza/api-estoque-produto",
+    codigo: "https://github.com/jsz611/product-stock-api",
   },
 ];
